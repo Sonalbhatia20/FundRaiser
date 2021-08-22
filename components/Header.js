458defa34a5d9571d7cb1dba525ca4949b327631
Sonalbhatia20 <sonalbhatia2087@gmail.com>
2021-08-22 15:09:37 +0530

    Update

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
       <Link route="/">
-        <a className="item">CrowdCoin</a>
+        <a className="item">FundRaiser</a>
       </Link>
       <Menu.Menu position="right">
         <Link route="/">
